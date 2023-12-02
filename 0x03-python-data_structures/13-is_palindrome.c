a47195d72d113d74d255e52d901fa8ee1c4d1b39
benson <ibeabuchibenson@gmail.com>
2023-12-02 12:35:38 -0800

    palidrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,99 +1,87 @@
+#include "lists.h"
 #include <stdio.h>
-#include <stdlib.h>
 
-/* Definition for singly-linked list node */
-typedef struct listint_t {
-    int data;
-    struct listint_t* next;
-} listint_t;
+void reverse_list(listint_t **head);
+int list_equiv(listint_t *l1, listint_t *l2);
 
-/* Function to check if a linked list is a palindrome */
-int is_palindrome(listint_t **head) {
-    if (*head == NULL || (*head)->next == NULL) {
-        return 1;  // An empty list or a single-node list is a palindrome
-    }
+/**
+ * is_palindrome - checks if a linked list is a palindrome
+ * @head: double pointer to the head of the linked list
+ *
+ * Return: 0 (not palindrome) 1 (is palindrome)
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *skip_1, *skip_2, *prev_s1, *first_half, *second_half, *mid;
 
-    listint_t *slow = *head, *fast = *head;
-    
-    // Move 'fast' to the end and 'slow' to the middle of the list
-    while (fast != NULL && fast->next != NULL) {
-        fast = fast->next->next;
-        slow = slow->next;
-    }
+	/* linked lists of length 0 and 1 are palindromes by default */
+	if (!head || !(*head) || !((*head)->next))
+		return (1);
 
-    // Reverse the second half of the list
-    listint_t *prev = NULL, *curr = slow, *next = NULL;
-    while (curr != NULL) {
-        next = curr->next;
-        curr->next = prev;
-        prev = curr;
-        curr = next;
-    }
+	first_half = skip_1 = skip_2 = prev_s1 = *head;
+	second_half = mid = NULL;
 
-    // Compare the first and reversed second halves
-    listint_t *first = *head, *second = prev;
-    while (second != NULL) {
-        if (first->data != second->data) {
-            return 0;  // Not a palindrome
-        }
-        first = first->next;
-        second = second->next;
-    }
+	while (skip_1 && skip_2 && skip_2->next)
+	{
+		prev_s1 = skip_1;
+		skip_1 = skip_1->next;
+		skip_2 = skip_2->next->next;
+	}
+	if (skip_2 == NULL) /* Even # of nodes */
+		second_half = skip_1;
+	else /* odd number of nodes, there is a middle node */
+	{
+		mid = skip_1;
+		second_half = skip_1->next;
+	}
+	prev_s1->next = NULL; /* null terminate first half */
+	reverse_list(&second_half);
 
-    return 1;  // Palindrome
+	if (list_equiv(first_half, second_half))
+		return (1); /* equivalent lists, palindrome found */
+	else
+		return (0);
 }
 
-/* Function to create a new node with given data */
-listint_t *createNode(int data) {
-    listint_t *newNode = (listint_t *)malloc(sizeof(listint_t));
-    if (newNode == NULL) {
-        fprintf(stderr, "Memory allocation error\n");
-        exit(EXIT_FAILURE);
-    }
-    newNode->data = data;
-    newNode->next = NULL;
-    return newNode;
+/**
+ * list_equiv - checks if two linked lists contain identical data and are
+ * the same length as each other
+ * @l1: list one to compare to list two
+ * @l2: list two to compare to list one
+ *
+ * Return: 1 (equivalent) 0 (not equal)
+ */
+int list_equiv(listint_t *l1, listint_t *l2)
+{
+	while (l1 || l2)
+	{
+		if (l1->n != l2->n || !l1 || !l2)
+			return (0);
+		if (l1)
+			l1 = l1->next;
+		if (l2)
+			l2 = l2->next;
+	}
+	return (1);
 }
 
-/* Function to print the linked list */
-void printList(listint_t *head) {
-    while (head != NULL) {
-        printf("%d -> ", head->data);
-        head = head->next;
-    }
-    printf("NULL\n");
+/**
+ * reverse_list - reverses a linked list
+ * @head: double pointer to head of linked list so we can modify it
+ *
+ * Return: always void, modifies head itself.
+ */
+void reverse_list(listint_t **head)
+{
+	listint_t *next = NULL, *prev = NULL, *cur;
+
+	cur = *head;
+	while (cur)
+	{
+		next = cur->next;
+		cur->next = prev;
+		prev = cur;
+		cur = next;
+	}
+	*head = prev;
 }
-
-/* Function to free the memory allocated for the linked list */
-void freeList(listint_t *head) {
-    listint_t *temp;
-    while (head != NULL) {
-        temp = head;
-        head = head->next;
-        free(temp);
-    }
-}
-
-/* Example usage */
-int main() {
-    listint_t *head = createNode(1);
-    head->next = createNode(2);
-    head->next->next = createNode(3);
-    head->next->next->next = createNode(2);
-    head->next->next->next->next = createNode(1);
-
-    printf("Original list: ");
-    printList(head);
-
-    if (is_palindrome(&head)) {
-        printf("The list is a palindrome.\n");
-    } else {
-        printf("The list is not a palindrome.\n");
-    }
-
-    // Free the allocated memory
-    freeList(head);
-
-    return 0;
-}
-
